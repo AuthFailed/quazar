7ec584d181e139c0779535b6405f1e2255544ef2
Roman Chursanov <authfailed@gmail.com>
2025-01-20 10:11:19 +0500

    Добавлена инструкция для v2rayNG на Android

--- tgbot/handlers/users/instructions.py
+++ tgbot/handlers/users/instructions.py
@@ -22,13 +22,14 @@ async def ios_app_instructions(callback: CallbackQuery) -> None:
     device = callback.data.split('_')[0]
     user = await get_user_by_id(user_id=callback.from_user.id)
 
+    message = ""
     if app == "hiddify":
         message = f"""<b>⭐ Квазар | Инструкция для iOS / Hiddify</b>
 
-<b>🐥 Простой вариант</b>
+<b>👨‍🔧 Установка</b>
 1. Установи приложение <a href="https://apps.apple.com/us/app/hiddify-proxy-vpn/id6596777532">Hiddify</a>
 2. Запусти приложение, выбери регион по инструкции ниже
-3. <a href="{user.subscription_url}">Нажми сюда</a>, найди iOS - Hiddify и нажми <code>Добавить в приложение</code>
+3. Открой подписку, найди iOS - Hiddify и нажми <code>Добавить в приложение</code>
 
 <b>🦾 Продвинутый вариант</b>
 1. Установи приложение <a href="https://apps.apple.com/us/app/hiddify-proxy-vpn/id6596777532">Hiddify</a>
@@ -42,8 +43,8 @@ async def ios_app_instructions(callback: CallbackQuery) -> None:
 2. Регион Другой: Все адреса/сайты <b>через VPN</b>
 """
 
-        await callback.message.edit_text(message,
-                                         reply_markup=back_to_apps(device=device), disable_web_page_preview=True)
+    await callback.message.edit_text(message, reply_markup=back_to_apps(device=device, sub_link=user.subscription_url),
+                                     disable_web_page_preview=True)
 
 
 @user_instructions.callback_query(lambda c: c.data.startswith("android_app_"))
@@ -58,25 +59,27 @@ async def android_app_instructions(callback: CallbackQuery) -> None:
     device = callback.data.split('_')[0]
     user = await get_user_by_id(user_id=callback.from_user.id)
 
+    message = ""
     if app == "hiddify":
         message = f"""<b>⭐ Квазар | Инструкция для Android / Hiddify</b>
 
-<b>🐥 Простой вариант</b>
+<b>👨‍🔧 Установка</b>
 1. Установи приложение <a href="https://play.google.com/store/apps/details?id=app.hiddify.com">Hiddify</a>
 2. Запусти приложение, выбери регион по инструкции ниже
-3. <a href="{user.subscription_url}">Нажми сюда</a>, найди Android - Hiddify и нажми <code>Добавить в приложение</code>
-
-<b>🦾 Продвинутый вариант</b>
-1. Установи приложение <a href="https://play.google.com/store/apps/details?id=app.hiddify.com">Hiddify</a>
-2. Запусти приложение, выбери регион по инструкции ниже
-3. Скопируй ссылку ниже
-<code>{user.subscription_url}</code>
-4. В приложении вверху справа нажми на ➕ и <code>Добавить из буфера обмена</code>
+3. Открой подписку, найди Android - Hiddify и нажми <code>Добавить в приложение</code>
 
 <b>🎌 Выбор региона</b>
 1. Россия: Российские адреса/сайты - <b>без VPN</b>, остальное - <b>через VPN</b>
 2. Другой: Все адреса/сайты <b>через VPN</b>
 """
+    elif app == "v2rayng":
+        message = f"""<b>⭐ Квазар | Инструкция для Android / v2rayNG</b>
 
-        await callback.message.edit_text(message,
-                                         reply_markup=back_to_apps(device=device), disable_web_page_preview=True)
+<b>👨‍🔧 Установка</b>
+1. Установи приложение <a href="https://play.google.com/store/apps/details?id=com.v2ray.ang">v2rayNG</a>
+2. Открой подписку, найди Android - V2RayNG и нажми <code>Добавить в приложение</code>
+3. В приложении нажми на три точки справа сверху и выбери <code>Обновить подписку группы</code>
+"""
+
+    await callback.message.edit_text(message, reply_markup=back_to_apps(device=device, sub_link=user.subscription_url),
+                                     disable_web_page_preview=True)
